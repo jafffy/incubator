0f9660d70b3a6b6e2ecdbe45488bf52f3c951390
Sung Young Min <sym6288@naver.com>
2015-11-18 00:08:35 +0900

    filled oled draw

--- src/lib/liboled.c
+++ src/lib/liboled.c
@@ -41,19 +41,44 @@ static int writeData(int size , unsigned char* dataArr)
 
 }
 
-void oled_open()//드라이버 오픈
+void oled_open()//open the device driver
 {
 	context.fd = open(OLED_DRIVER_PATH,O_RDWR);
 }
 
-void oled_destroy()//드라이버 클로즈
+void oled_destroy()//close the device driver
 {
 	close(oledCont.fd);
 }
 
-int oled_imageLoading(const char* fileName)
+int oled_imageLoading(const char* fileName)//load the image and put fileName 
 {
+	int imgfile;
+	unsigned char* temp =NULL;//임시 보관
+	int  width , height;
 
+	imgfile = open(fileName , O_RDONLY );
+	if ( imgfile < 0 ) 
+	{
+		printf ("imageloading(%s)  file is not exist . err.\n",fileName);
+		return FALSE;
+	}
+	setCmdLock(FALSE);//명령어 입력 잠금 해제
+
+
+	read(imgfile ,&width , sizeof(unsigned char));
+	read(imgfile ,&height , sizeof(unsigned char));
+
+	temp = malloc( 128 * 128 * 3 );//메모리 확보
+
+	read(imgfile, temp , 128 * 128 *3 );//이미지 데이터를 템프로 복사
+
+	close(imgfile);//사용한 이미지 파일 클로즈
+
+	writeData(128 * 128 *3 ,temp );//oled에 이미지 로딩
+
+	setCmdLock(TRUE);//명령어 입력 잠금
+	return TRUE;
 }
 
 
